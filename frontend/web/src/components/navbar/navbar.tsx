894af0a92aa72a4230a6ec211140826c370392af
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-10 23:22:18 -0300

    finalize home page

--- frontend/web/src/components/navbar/navbar.tsx
+++ frontend/web/src/components/navbar/navbar.tsx
@@ -3,19 +3,25 @@ import './style.css';
 
 function NavBar() {
   return (
-    <div className="nav">
-      <ul>
-        <li>
-          <div className="buttonContainer">
-            <p className="buttonText">Home</p>
-          </div>
-        </li>
-        <li>
-          <div className="buttonContainer">
-            <p className="buttonText">Home</p>
-          </div>
-        </li>
-      </ul>
+    <div>
+      <div className="nav">
+        <div className="buttonContainer" id="homeButton">
+          <p className="buttonText">Home</p>
+        </div>
+        <div className="buttonContainer">
+          <p className="buttonText">About US</p>
+        </div>
+        <div className="buttonContainer">
+          <p className="buttonText">Galery</p>
+        </div>
+        <div className="buttonContainer">
+          <p className="buttonText">Curiosity</p>
+        </div>
+      </div>
+      <div className="getStartedContainer">
+        <p>Discover the solar system in a way you've never seen.</p>
+        <button>Get Started</button>
+      </div>
     </div>
   );
 }
