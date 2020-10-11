2f245f1fd487823f6dc3f70a9be367cdc83975de
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-10 21:25:54 -0300

    start new project

--- frontend/web/src/routes.tsx
+++ frontend/web/src/routes.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-
 import { BrowserRouter, Route } from 'react-router-dom';
-// import App from './App';
+
 import Home from './pages/home/index';
-import splash from './pages/splashScreen/index';
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={splash} />
-      <Route path="/home" component={Home} />
+      <Route path="/" exact component={Home} />
     </BrowserRouter>
   );
 }
